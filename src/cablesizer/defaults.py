94fa82ce57735d35ccf26c60888632d19225f49d
tomackl <tomackl@users.noreply.github.com>
2020-06-27 22:38:05 +1000

    20200625 cable.CableSpec tests added

--- src/cablesizer/defaults.py
+++ src/cablesizer/defaults.py
@@ -7,7 +7,51 @@ class CableRun:
     A class to store the default values for the cable_list run. The default values are stored in the "cable.json" file
     as a JSON object.
     """
-    def __init__(self, fp):
+    def __init__(self, fp, single_phase_arrangement: list = [], single_phase_name: str = None,
+                 single_phase_abbreviation: str = None, single_phase_default: str = None,
+                 multi_phase_arrangement: list = [], multi_phase_name: str = None, multi_phase_abbreviation: str = None,
+                 multi_phase_default: str = None, control_arrangement: list = [], control_name: str = None,
+                 control_abbreviation: str = None, control_default: str = None, instrument_pair_arrangement: list = [],
+                 instrument_pair_name: str = None, instrument_pair_abbreviation: str = None,
+                 instrument_pair_default: str = None, instrument_triple_arrangement: list = [],
+                 instrument_triple_name: str = None, instrument_triple_abbreviation: str = None,
+                 instrument_triple_default: str = None, shape_description: list = [], shape_default: str = None,
+                 conductor_material_description: list = [], conductor_material_default: str = None):
+        """
+
+        :param fp:
+        :param single_phase_arrangement:
+        :param single_phase_name:
+        :param single_phase_abbreviation:
+        :param single_phase_default:
+        :param multi_phase_arrangement:
+        :param multi_phase_name:
+        :param multi_phase_abbreviation:
+        :param multi_phase_default:
+        :param control_arrangement:
+        :param control_name:
+        :param control_abbreviation:
+        :param control_default:
+        :param instrument_pair_arrangement:
+        :param instrument_pair_name:
+        :param instrument_pair_abbreviation:
+        :param instrument_pair_default:
+        :param instrument_triple_arrangement:
+        :param instrument_triple_name:
+        :param instrument_triple_abbreviation:
+        :param instrument_triple_default:
+        """
+        self.single_phase = CoreArrangement(single_phase_arrangement, single_phase_name, single_phase_abbreviation,
+                                            single_phase_default)
+        self.multi_phase = CoreArrangement(multi_phase_arrangement, multi_phase_name, multi_phase_abbreviation,
+                                           multi_phase_default)
+        self.control = CoreArrangement(control_arrangement, control_name, control_abbreviation, control_default)
+        self.instrument_pair = CoreArrangement(instrument_pair_arrangement, instrument_pair_name,
+                                               instrument_pair_abbreviation, instrument_pair_default)
+        self.instrument_triplex = CoreArrangement(instrument_triple_arrangement, instrument_triple_name,
+                                                  instrument_triple_abbreviation, instrument_triple_default)
+        self.shape = BasicDefaults(shape_description, shape_default)
+        self.conductor_material = BasicDefaults(conductor_material_description, conductor_material_default)
         self._cable = None
         self._cct = None
         self._install_method = None
@@ -63,3 +107,131 @@ class CableRun:
     @power_unit.setter
     def power_unit(self, value: dict):
         self._pwr_unit = value
+
+
+class CoreArrangement:
+    def __init__(self, description: list = "", name: str = None, abbreviation: str = None, default: str = None):
+        """
+
+        :param description:
+        :param name:
+        :param abbreviation:
+        :param default:
+        """
+        self.description = description
+        self.name = name
+        self.abbreviation = abbreviation
+        self.default = default
+
+    @property
+    def description(self) -> list:
+        return self._descrip
+
+    @description.setter
+    def description(self, value: list):
+        self._descrip = value
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        if value is not None:
+            self._name = value.upper()
+        else:
+            self._name = value
+
+    @property
+    def abbreviation(self) -> str:
+        return self._abbreviation
+
+    @abbreviation.setter
+    def abbreviation(self, value: str):
+        if value is not None:
+            self._abbreviation = value.upper()
+        else:
+            self._abbreviation = value
+
+    @property
+    def default(self) -> str:
+        return self._default.upper()
+
+    @default.setter
+    def default(self, value: str):
+        if value is None:
+            self._default = value
+        elif value.upper() in self.description:
+            self._default = value.upper()
+        # elif value is None:
+        #     self._default = value
+        else:
+            raise ValueError(f"Value ({value}) not contained in description list")
+
+    def to_dict(self) -> dict:
+        """
+        Return the class as a dictionary.
+        :return:
+        """
+        return {"description": self.description,
+                "name": self.name,
+                "abbreviation": self.abbreviation,
+                "default": self.default
+                }
+
+    def from_dict(self, value: dict):
+        """
+        Populate the class via a dictionary
+        :param value:
+        :return:
+        """
+        self.description = value["description"]
+        self.name = value["name"]
+        self.abbreviation = value["abbreviation"]
+        self.default = value["default"]
+
+
+class BasicDefaults:
+    def __init__(self, description: list = [], default: str = None):
+        self.description = description
+        self.default = default
+
+    @property
+    def description(self) -> list:
+        return self._descrip
+
+    @description.setter
+    def description(self, value: list):
+        if value is not None:
+            self._descrip = value
+
+    @property
+    def default(self) -> str:
+        return self._default.upper()
+
+    @default.setter
+    def default(self, value: str):
+        if value is None:
+            self._default = value
+        elif value.upper() in self.description:
+            self._default = value.upper()
+        else:
+            raise ValueError(f"Value ({value}) not contained in description list")
+
+    def to_dict(self) -> dict:
+        """
+        Return the class as a dictionary.
+        :return:
+        """
+        return {"description": self.description,
+                "default": self.default
+                }
+
+    def from_dict(self, value: dict):
+        """
+        Populate the class via a dictionary
+        :param value:
+        :return:
+        """
+        self.description = value["description"]
+        self.default = value["default"]
--- tests/unit/test_defaults.py
+++ tests/unit/test_defaults.py
@@ -4,6 +4,49 @@ import pprint as pp
 
 
 def test_cls_cable_run_defaults():
-    test_class = defaults.CableRun("../../src/Constants/cable.json")
-    for each in [test_class.cable, test_class.circuit, test_class.installation, test_class.power_unit]:
-        pp.pprint(each)
+    # test_class = defaults.CableRun("../../src/Constants/cable.json")
+    pass
+    # for each in [test_class.cable, test_class.circuit, test_class.installation, test_class.power_unit]:
+    #     pp.pprint(each)
+
+
+def test_cls_core_arrangement_defaults():
+    test_class = defaults.CoreArrangement()
+    test_class.from_dict({"description": ["1TRI", "2TRI", "4TRI"],
+                          "name": "triple",
+                          "abbreviation": "tri",
+                          "default": "1tri"}
+                         )
+    expected = {"description": ["1TRI", "2TRI", "4TRI"],
+                "name": "TRIPLE",
+                "abbreviation": "TRI",
+                "default": "1TRI"}
+    result = test_class.to_dict()
+    assert result == expected
+
+
+def test_cls_core_arrangement_exception():
+    test_class = defaults.CoreArrangement()
+    with pytest.raises(ValueError):
+        test_class.from_dict({"description": ["1TRI", "2TRI", "4TRI"],
+                              "name": "triple",
+                              "abbreviation": "tri",
+                              "default": "1pr"}
+                             )
+
+
+def test_cls_basic_defaults():
+    test_class = defaults.BasicDefaults()
+    test_class.from_dict({"description": ["CIRCULAR", "FLAT"],
+                          "default": "circular"})
+    expected = {"description": ["CIRCULAR", "FLAT"],
+                "default": "CIRCULAR"}
+    result = test_class.to_dict()
+    assert result == expected
+
+
+def test_cls_basic_defaults_exception():
+    test_class = defaults.BasicDefaults()
+    with pytest.raises(ValueError):
+        test_class.from_dict({"description": ["CIRCULAR", "FLAT"],
+                              "default": "triangle"})
